b51f7a3ad3ee24406ba21e114f764de81ef154e2
BayuTriwibowo18 <144087130+BayuTriwibowo18@users.noreply.github.com>
2024-04-25 12:25:52 +0700

    Add files via upload

---
+++ Tugas07/Pasien07.java
@@ -0,0 +1,15 @@
+package Tugas07;
+
+public class Pasien07 {
+    String nama;
+    int noID;
+    char jenisKelamin;
+    int umur;
+
+    public Pasien07(String nama, int noID, char jenisKelamin, int umur) {
+        this.nama = nama;
+        this.noID = noID;
+        this.jenisKelamin = jenisKelamin;
+        this.umur = umur;
+    }
+}
